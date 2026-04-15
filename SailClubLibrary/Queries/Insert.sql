5ea72f9907cddab0f68425d7df2c82eee6ca945c
Neozt001 <neo001@edu.zealand.dk>
2026-04-15 15:13:37 +0200

    Minor Changes

--- SailClubLibrary/Queries/Insert.sql
+++ SailClubLibrary/Queries/Insert.sql
@@ -23,8 +23,9 @@ VALUES ('16-1111', '1', 20, 10, 30, 1980, 'Good', 1, DEFAULT),
 USE SailClubDatabase;
 GO
 INSERT INTO Members
-VALUES ('Poul', 'Hansen', '10101010', 'Gade 123', 'København', 'Mail01@gmail.dk', 1, 1, DEFAULT, DEFAULT),
-('Per', 'Hansen', '20202020', 'Gade 123', 'København', 'Mail02@gmail.dk', 2, 2, DEFAULT, DEFAULT);
+VALUES ('Poul', 'Hansen', '10101010', 'Gade 1', 'Roskilde', 'Mail01@gmail.dk', 1, 1, DEFAULT, DEFAULT),
+('Per', 'Hansen', '20202020', 'Gade 2', 'København', 'Mail02@gmail.dk', 2, 2, DEFAULT, DEFAULT),
+('Hans', 'Hansen', '30303030', 'Gade 3', 'Hillerød', 'Mail03@gmail.dk', 2, 0, DEFAULT, DEFAULT);
 
 INSERT INTO Bookings
 VALUES ('2020-01-01', '2020-02-02', 'Destination A', 1, 1);
